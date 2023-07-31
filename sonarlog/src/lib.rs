5071d4af28559e0f883e2d23216e0e0d5b88fd28
Lars T Hansen <larstha@uio.no>
2023-07-31 14:50:30 +0200

    Clean up documentation

--- sonarlog/src/lib.rs
+++ sonarlog/src/lib.rs
@@ -6,14 +6,7 @@
 //    transparent provided the caching is per-user and the user running the log processing has a
 //    home directory and write access to it.
 //
-//  - This will transparently deal with old (untagged) and new (tagged) log file formats, and will
-//    likely evolve to indicate, for each field (though possibly only for some fields), whether the
-//    field is present in a record or not.
-//
 //  - Hostname filtering (beyond FQDN matching) must be implemented in logtree.md.
-//
-//  - The aggregate structure does not have fields for absolute vmem, and there are some things to
-//    document re how vmem is exposed on various hardware
 
 mod dates;
 mod jobs;
@@ -21,8 +14,12 @@ mod load;
 mod logfile;
 mod logtree;
 
-// Create a set of plausible log file names within a directory tree, for a date range and a set of
-// included host files.
+use chrono::prelude::DateTime;
+use chrono::Utc;
+use std::collections::HashSet;
+
+// Compute a set of plausible log file names within a directory tree, for a date range and a set of
+// included host names.
 
 pub use logtree::find_logfiles;
 
@@ -31,37 +28,62 @@ pub use logtree::find_logfiles;
 
 pub use logfile::parse_logfile;
 
-// Create a map from job ID to a vector of all the records for the job sorted ascending by
-// timestamp, and return that map along with metadata about the unfiltered records.
-
-pub use jobs::compute_jobs;
-
-use chrono::prelude::DateTime;
-use chrono::Utc;
-use std::collections::HashSet;
-
 /// The LogEntry structure holds slightly processed data from a log record: Percentages have been
 /// normalized to the range [0.0,1.0], and memory sizes have been normalized to GB.
 
 #[derive(Debug)]
 pub struct LogEntry {
+    /// Format "major.minor.bugfix"
     pub version: String,
+
+    /// The time is common to all records created by the same sonar invocation.  It has no subsecond
+    /// precision.
     pub timestamp: DateTime<Utc>,
+
+    /// Fully qualified domain name.
     pub hostname: String,
+    
+    /// Number of cores on the node.  This is never zero.
     pub num_cores: u32,
+
+    /// Unix user name, or "_zombie_something" or "_unknown_"
     pub user: String,
+
+    /// The job_id is ideally never zero, but sometimes it will be if no job ID can be computed.
     pub job_id: u32,
+
+    /// The command contains at least the executable name.  It may contain spaces and other special
+    /// characters.
     pub command: String,
+
+    /// For CPU usage, 1.0 means 1 full core's worth.
     pub cpu_pct: f64,
+
+    /// Main memory used by the job on the node (the memory is shared by all cores on the node).
     pub mem_gb: f64,
-    pub gpus: Option<HashSet<u32>>, // None for "none", empty set for "unknown", otherwise the precise set
+
+    /// The set of GPUs used by the job on the node, None for "none", Some({}) for "unknown",
+    /// otherwise Some({m,n,...}).
+    pub gpus: Option<HashSet<u32>>,
+
+    /// Percent of the sum of the capacity of all GPUs in `gpus`.
     pub gpu_pct: f64,
+
+    /// Percent of the sum of the capacity of all GPUs in `gpus`.  Note this is not always
+    /// reliable.
     pub gpu_mem_pct: f64,
+
+    /// Memory usage across all GPUs in `gpus`.  Note this is not always reliable.
     pub gpu_mem_gb: f64,
 }
 
-/// Create a map from host name to a vector of maps from time stamp to all the records for that time,
-/// return the maps sorted ascending by host name and time.
+// Create a map from job ID to a vector of all the records for the job sorted ascending by
+// timestamp, and return that map along with metadata about the unfiltered records.
+
+pub use jobs::compute_jobs;
+
+// Create a map from host name to a vector of maps from time stamp to all the records for that time,
+// return the maps sorted ascending by host name and time.
 
 pub use load::compute_load;
 
