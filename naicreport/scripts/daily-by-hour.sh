c69073ea3a64668cbbe4571f514cd7b65caee8d6
Lars T Hansen <larstha@uio.no>
2023-09-15 08:28:11 +0200

    Generation scripts

---
+++ naicreport/scripts/daily-by-hour.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# This is meant to be run in a directory that has everything, symlinked if necessary
+
+mkdir -p ./output
+./naicreport ml-webload -tag daily -sonalyze ./sonalyze -config-file ./ml-nodes.json -output-path ./output -data-path ./data
---
+++ naicreport/scripts/monthly-by-day.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# This is meant to be run in a directory that has everything, symlinked if necessary
+
+mkdir -p ./output
+./naicreport ml-webload -tag monthly -sonalyze ./sonalyze -config-file ./ml-nodes.json -output-path ./output -data-path ./data -from 30d -daily
+
