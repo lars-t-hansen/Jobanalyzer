f8b5ac1623abfc5259731c70a13601c7b564965a
Lars T Hansen <larstha@uio.no>
2023-09-19 11:38:32 +0200

    Remove dead scripts

--- naicreport/mlwebload/scripts/quarterly-by-day.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# This is meant to be run in a directory that has everything, symlinked if necessary
-
-mkdir -p ./output
-./naicreport ml-webload -tag quarterly -sonalyze ./sonalyze -config-file ./ml-nodes.json -output-path ./output -data-path ./data -from 90d -daily
-
